deda0f109873731efcfdec4807d3eb9687ccea46
sahil kushwaha <sahilkushwaha5510@gmail.com>
2024-03-06 21:47:14 +0530

    add log file and change port 8000 to 8080

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
 const http= require ('http')
+const fs=require('fs')
 
-const server=http.createServere((req ,res)=>{
-       
-       const url =req.url()
-           if(url=="/"){
-               res.end("you are at home");
-           }
+const server=http.createServer((req ,res)=>{
+     const log =`At ${Date.now()}: ${req.method} request at ${req.url}\n`
+       fs.appendFile("log.txt",log,()=>{
+        const url =req.url
+        if(url=="/"){
+            res.end("you are at home");
+        }
 
-           if(requestUrl=='/about')
-           {
-              res.end("sahil here");
-           }
-               
-           if(url=='/contact'){
-            res.end("Address:xyz\n mob: 342094");
-          }
+        if(url=='/about')
+        {
+           res.end("sahil here");
+        }
+            
+        if(url=='/contact'){
+         res.end("Address:xyz\n mob: 342094");
+       }
+
+       })
+      
 })
 
-const port=8000
+const port=8080
 server.listen(port,(err)=>{
     console.log(`server started at ${port}`)
 })
